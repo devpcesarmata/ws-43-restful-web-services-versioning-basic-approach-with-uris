d102ad303273837ef0b3be287c490c285ffa8874
César <dev.pcesarmata@gmail.com>
2020-09-11 11:02:30 -0500

    Init commit.

---
+++ src/test/java/com/cesarmata/Ws43RestfulWebServicesVersioningBasicApproachWithUrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws43RestfulWebServicesVersioningBasicApproachWithUrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
